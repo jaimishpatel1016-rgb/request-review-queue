64bb138659941b7c42a984dad3fb76e7b4c2daef
jaimishpatel1016-rgb <jaimishpatel1016@gmail.com>
2026-04-21 14:29:33 +0530

    feat: add dotenv for environment variable management and added the mongoDB connection function

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
+dotenv.config();
+
+import { connectDB } from "./db.js";
 
 const app: express.Express = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
@@ -11,8 +15,14 @@ app.get("/health", (_req, res) => {
   res.status(200).send("Server is healthy");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch(() => {
+    process.exit(1);
+  });
 
 export default app;
